6cdc812f729a850ca56180ed41f2e029a0271955
Tyler Morris <tylermorris@TylerM-MBP.local>
2013-04-14 13:30:30 -0400

    changes

--- FilePartitioner/src/FilePartitioner.java
+++ FilePartitioner/src/FilePartitioner.java
@@ -1,9 +1,35 @@
 import java.util.Vector;
+import java.io.*;
+import java.nio.file.*;
 
-
+import javax.swing.JOptionPane;
 public class FilePartitioner {
 
-	byte[] FileArray;
-	Vector<byte[]> outputArray;
-	Vector<String> outputFiles;
+	static byte[] fileArray;
+	static Vector<byte[]> outputArray;
+	static Vector<Path> outputFiles;
+	
+	//need file manager
+	static boolean partitionFile(Path  file)
+	{
+		/***********************************************************************
+		 * Convert file to byte array
+		 **********************************************************************/
+		try {
+			fileArray = Files.readAllBytes(file);
+		} catch (IOException e) {
+			fileReadError();
+		}
+		
+		
+		return false;
+	}
+	/***************************************************************************
+	 * Error when file cannot be read to fileArray
+	 **************************************************************************/
+	private static void fileReadError()
+	{
+		JOptionPane.showMessageDialog(null, "Failed to read input file.",  
+				"Results", JOptionPane.ERROR_MESSAGE);
+	}
 }
